411f0dd378e421ea6048f997b4479a70f6d17054
fullergalway <1871541+fullergalway@users.noreply.github.com>
2021-11-12 20:24:56 +0000

    updated to go live

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Link from "@mui/material/Link";
-import { Motion, spring } from "react-motion";
-import CookieConsent from "react-cookie-consent";
+
+import MenuIcon from '@mui/icons-material/Menu';
 import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Container from "@mui/material/Container";
+import Fab from '@mui/material/Fab';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import Link from "@mui/material/Link";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from "@mui/material/Typography";
+import CookieConsent from "react-cookie-consent";
+import ReportProblemIcon from '@mui/icons-material/ReportProblem';
+import { Motion, spring } from "react-motion";
 
 function Copyright() {
   return (
@@ -20,53 +33,143 @@ function Copyright() {
   );
 }
 
+function MyAppBar() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            id="positioned-button"
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            aria-controls="positioned-menu"
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="positioned-menu"
+            aria-labelledby="positioned-button"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+          >
+            <MenuItem component="a"  href="https://twitter.com/notmisleading" target="_blank">
+              <ListItemIcon>
+                <TwitterIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Follow on Twitter</ListItemText>
+            </MenuItem>
+            <MenuItem component="a"  href="https://github.com/fullergalway/snitch.ie" target="_blank">
+              <ListItemIcon>
+                <GitHubIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Improve this on Github</ListItemText>
+            </MenuItem>
+          </Menu>
+
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Safe Neighbourhood Incident Tracking Covid-19 Hotline
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
 export default function App() {
   const [open, setOpen] = useState(false);
   const [randX, setRandX] = useState(0);
   const [randY, setRandY] = useState(0);
+  const [offset, setOffset] = useState({ x: 0, y: 0 })
 
   const move = (e) => {
     e.preventDefault();
-    setOpen(true || !open);
-    setRandX(Math.floor(Math.random() * (window.innerWidth - 400)));
-    setRandY(Math.floor(Math.random() * (window.innerHeight - 100)));
+    if (!open) {
+      setOpen(true);
+      const rect = e.target.getBoundingClientRect();
+      setOffset({ x: rect.left, y: rect.top });
+    }
+    setRandX(Math.floor(Math.random() * (window.innerWidth - 200)) - offset.x);
+    setRandY(Math.floor(Math.random() * (window.innerHeight - 100)) - offset.y);
   };
 
   return (
     <>
-      <div style={{ width: "100%", height: "100%" }}>
-        <Container fixed>
-          <Typography variant="h4" component="h2" gutterBottom>
-            Public health science violation reporting system.
-          </Typography>
-          <Typography variant="h5" component="h2" gutterBottom>
-            Please help keep everyone safe.
-          </Typography>
-          <Button variant="outlined" href="https://twitter.com/notmisleading">
-              <TwitterIcon /> Keep Updated
-          </Button><br />
+      <MyAppBar />
+      <Container fixed justifyContent="center">
+        <Grid container spacing={3} justifyContent="center">
+        <Grid item xs={10} sm={9} md={8} lg={6} ><Typography variant="h5" component="h1" gutterBottom>
+          Welcome to the public health science violation reporting system.
+        </Typography></Grid>
+        <Grid item xs={12}></Grid>
+          <Grid item xs={10} sm={9} md={8} lg={6} ><Typography variant="body1" component="p">
+            In these dangerous times it is especially important that we work together to help keep everyone safe.
+          </Typography></Grid>
+          <Grid item xs={12}></Grid>
+          <Grid item xs={10} sm={9} md={8} lg={6} >
+            <Typography variant="body1" component="p" >
+              Public health directives always follow the science.
+              Because science evolves over time, the guidance changes frequenly too.
+              When the rules make no sense at all, that is when following them becomes most important.
+            </Typography></Grid>
+          <Grid item xs={12}></Grid>
+          <Grid item xs={10} sm={9} md={8} lg={6} ><Typography variant="body1" component="p" >
+            The best way for citizens to keep each other safe is by following and upholding the latest rules, and helping others to do so too.
+          </Typography></Grid>
+          <Grid item xs={12}></Grid>
+          <Grid item xs={10} sm={9} md={8} lg={6} ><Typography variant="body1" component="p" >
+            Blatant violation of the science by anti-vax individuals and businesses following untrusted news sources <em>must be reported</em> to the authorities.
+          </Typography></Grid>
+
+        <Grid item xs={12} container justifyContent="center">
           <Motion
             style={{ x: spring(open ? randX : 0), y: spring(open ? randY : 0) }}
           >
             {({ x, y }) => (
-              <Button
+              <Fab
                 style={{
                   position: "absolute",
                   WebkitTransform: `translate3d(${x}px, ${y}px, 0)`,
                   transform: `translate3d(${x}px, ${y}px, 0)`,
                 }}
-                variant="outlined"
+                variant="extended"
+                color="primary"
                 onMouseDown={move}
                 onMouseOver={move}
                 onTouchStart={move}
               >
+                <ReportProblemIcon sx={{ mr: 1 }} />
                 Report Non-Compliance
-              </Button>
+              </Fab>
             )}
           </Motion>
+        </Grid>
+        </Grid>
 
-        </Container>
-      </div>
+      </Container>
 
       <CookieConsent>
         This website uses cookies to help keep everyone safe.
